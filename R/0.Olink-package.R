2911f020c6cbd781e27876fd2e459ac1ff969347
Rundmus <mungwan@gmail.com>
2020-05-12 11:36:32 +0200

    Initialize Olink package

---
+++ R/0.Olink-package.R
@@ -0,0 +1,17 @@
+#' Olink : A package for Olink proteomics data
+#' 
+#' A collection of R functions to handle Olink proteomics data
+#' 
+#' @author Mun-Gwan Hong <\email{mungwan@gmail.com}>
+#' @keywords package
+#' 
+#' @docType package
+#' 
+#' @name Useful2me-package
+#' @import dplyr
+#' 
+NULL
+
+
+
+# GLOBAL CONSTANTS
---
+++ README.md
@@ -0,0 +1,4 @@
+# Olink-R_package
+
+A collection of R functions to handle Olink proteomics data
+
